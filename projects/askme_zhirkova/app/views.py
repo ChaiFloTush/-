71f2e290cb15a2351b577bd486e5014e925afed7
ChaiFloTush <ms.masha112371@gmail.com>
2023-11-09 12:52:11 +0300

    Created components folder

--- projects/askme_zhirkova/app/views.py
+++ projects/askme_zhirkova/app/views.py
@@ -4,7 +4,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    questions = [
+        {
+            'id': i,
+            'title': f'Question {i}',
+            'content': f'Long lorem ipsum {i}',
+            'grade': i,
+            'answers': f'Answers({i})',
+            'first_tag': f'first tag {i}',
+            'second_tag': f'second tag {i}'
+        } for i in range(10)
+    ]
+    return render(request, 'index.html', {'questions': questions})
 
 def question(request):
     return render(request, 'question.html')
