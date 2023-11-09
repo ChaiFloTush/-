b06e6016d6ee169deeb9f1b2bc2006adcae4cabc
ChaiFloTush <ms.masha112371@gmail.com>
2023-11-09 11:26:33 +0300

    Started making layouts

--- projects/askme_zhirkova/askme_zhirkova/urls.py
+++ projects/askme_zhirkova/askme_zhirkova/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app import views
+
 urlpatterns = [
+    path('', views.index),
+    path('question/', views.question),
     path('admin/', admin.site.urls),
 ]
+
